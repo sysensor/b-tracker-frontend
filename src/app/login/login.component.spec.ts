e9da5a6331a0d1ae367935f22d883183d43eccc7
Dinuka Malalanayake <dinuka.malalanayake@gmail.com>
2018-05-16 16:29:33 -0500

    Successful LoginComponent Test

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,5 +1,5 @@
 import {async, ComponentFixture, TestBed} from "@angular/core/testing";
-import {LoginComponent, LoginUser} from "./login.component";
+import {LoginComponent} from "./login.component";
 import {FormsModule} from "@angular/forms";
 import {RouterTestingModule} from "@angular/router/testing";
 import {TokenService} from "../service/token-service";
@@ -42,25 +42,14 @@ describe('LoginComponent', () => {
     it('Login UI should update the login user details', () => {
         document.getElementById('username').setAttribute("value", "admin");
         document.getElementById('password').setAttribute("value", "admin");
-        document.getElementById('login').click()
+
         //expect(component._service.user).toBe("admin");
         // expect(component._service.password).toBe("admin");
-        //spyOn(component._service, 'validateUserLogin').and.returnValue(Promise.resolve(true));
+        spyOn(component._service, 'validateUserLogin').and.returnValue(Promise.resolve(true));
+        document.getElementById('login').click();
+        expect(component._service.validateUserLogin).toHaveBeenCalled();
+        //expect(component._service.validateUserLogin).toBe(Promise.resolve(true));
 
     })
 });
 
-class MockTokenService extends TokenService {
-    public user: LoginUser;
-    constructor() {
-    }
-
-    validateUserLogin(loginDetails) {
-        this.user = loginDetails.username;
-        this.user.password = loginDetails.password;
-    }
-
-    public getUser() {
-        return this.user;
-    }
-}
